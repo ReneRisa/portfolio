bc8b612ad553ee05a036aa7275422b3a363e6995
ReneRisa <risael31@gmail.com>
2023-11-07 23:12:20 -0500

    Add some divs and bootstraps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='container'>
+        <header>
+          <div className='row'>
+            <div className='col-8'>
+              <div class="row justify-content-center align-items-center g-2">
+                <div class="col-12"><p className='text-white'>Rene Irias</p></div>
+                <div class="col-12"><p className='text-white'>Ovals</p></div>
+                <div class="col-12"><p className='text-white'>Nice to meet you! I'm Rene Irias.</p></div>
+                <div class="col-12"><p className='text-white'>Based in Canada, I'm a front-end developer passionate about building accessible web apps that users love.</p></div>
+                <div class="col-12"><p className='text-white'>Contact me</p></div>
+              </div>
+            </div>
+            <div className='col-4'>
+
+            </div>
+          </div>
+          <div className='text-wrapper'>Rene Irias</div>
+        </header>
+      </div>
     </div>
   );
 }
