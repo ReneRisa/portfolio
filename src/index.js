05349e40d9d57f2d2a4c1b891562fec239dc3b60
ReneRisa <risael31@gmail.com>
2023-11-09 22:39:19 -0500

    Contac Section created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+const btnSubmit = document.getElementById("btnSubmitForma");
+
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("SUBMIITED");
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  name.trim() == ""
+    ? (document.getElementById("nameError").style.display = "block")
+    : (document.getElementById("nameError").style.display = "none");
+  /*email.trim() == ""
+    ? (document.getElementById("emailError").style.display = "block")
+    : (document.getElementById("emailError").style.display = "none");*/
+  message.trim() == ""
+    ? (document.getElementById("messageError").style.display = "block")
+    : (document.getElementById("messageError").style.display = "none");
+  // Validate email
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email.trim() == "") {
+    document.getElementById("emailError").innerHTML = "Email is required!";
+    document.getElementById("emailError").style.display = "block";
+  } else if (!emailRegex.test(email)) {
+    document.getElementById("emailError").innerHTML = "Invalid email format";
+    document.getElementById("emailError").style.display = "block";
+  } else {
+    document.getElementById("emailError").innerHTML = "Email is required!";
+    document.getElementById("emailError").style.display = "none";
+  }
+
+  const data = {
+    name: name,
+    email: email,
+    message: message,
+  };
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
